3e94a9ed2f1f45995f3fefe85ed1ac9e23c2f594
Anirudh J <C201249@ardentmills.com>
2021-03-24 17:28:58 +0530

    Updated all the changes

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#0C1221',
+        headerTextColor: '#ffffff',
+      },
+      fontFamily: {
+        landingPageHeader: ['Stardos Stencil']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
